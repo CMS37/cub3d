78928132f1911776f964b013420243a5eb9c8a7b
rocky55737 <rocky55737@gmail.com>
2023-04-06 16:56:07 +0900

    leaks checked

--- srcs/get.c
+++ srcs/get.c
@@ -6,7 +6,7 @@
 /*   By: min-cho <min-cho@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/14 18:50:34 by min-cho           #+#    #+#             */
-/*   Updated: 2023/04/02 19:43:05 by min-cho          ###   ########seoul.kr  */
+/*   Updated: 2023/04/06 16:29:12 by min-cho          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ void	get_info(t_info *info, char **argv)
 	int		fd;
 	char	*line;
 	char	*tmp;
+	char	*tmp2;
 
 	fd = open(argv[1], O_RDONLY);
 	if (fd < 0)
@@ -29,7 +30,9 @@ void	get_info(t_info *info, char **argv)
 			break ;
 		if (ft_strcmp(tmp, "\n"))
 			info->size++;
-		line = ft_strjoin(line, tmp);
+		tmp2 = ft_strjoin(line, tmp);
+		free(line);
+		line = tmp2;
 		free(tmp);
 	}
 	info->map = ft_split(line, '\n');
